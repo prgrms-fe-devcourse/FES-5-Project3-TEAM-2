e2b416e289759d6e64af76d2ce4531ddfe9f7d6d
mintsky0172 <mintsky0172@naver.com>
2025-08-24 19:03:54 +0900

    임시 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -13,18 +12,3 @@ export default defineConfig({
     },
   },
 });
-=======
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
->>>>>>> main
