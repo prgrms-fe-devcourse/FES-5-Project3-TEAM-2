d79f707304ad53a8aec8b0ee5b8f01c33626cd1f
chechoii <choii2e@gmial.com>
2025-08-28 09:49:42 +0900

    fix: profile id추가

--- src/pages/Group/api/profile.ts
+++ src/pages/Group/api/profile.ts
@@ -4,7 +4,7 @@ import { supabase } from "@/lib/supabaseClient";
 export async function fetchProfileById(userId:string) {
     const { data, error } = await supabase
           .from("profile")
-          .select("name, avatar_url")
+          .select("id, name, avatar_url")
           .eq("id", userId)
           .single();
 
