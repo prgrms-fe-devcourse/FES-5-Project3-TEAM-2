f25cdaf053e3d6d181a7667d88fab6d87a9ffa40
chechoii <choii2e@gmial.com>
2025-08-29 12:52:55 +0900

    ui: 그룹카드 내에서 스크롤

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/common/Sidebar";
+import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <div className="grid grid-cols-[240px_1fr] h-dvh overflow-hidden bg-slate-50">
       <Sidebar />
-      <main className="p-6 min-h-0 overflow-hidden">
+      <main className="p-6 overflow-hidden">
         <Outlet />
       </main>
     </div>
