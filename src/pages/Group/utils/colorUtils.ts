187c2bc4dfad8d4277ef39e25ae89f84a649e6dc
HyoYoung0829 <gydud7300@naver.com>
2025-08-31 22:23:38 +0900

    ui(dashboard) : 현재 접속 아이콘 색상 초록색으로 통일

--- src/pages/Group/utils/colorUtils.ts
+++ src/pages/Group/utils/colorUtils.ts
@@ -1,14 +1,5 @@
 const COLORS = [
-  "text-pink-300",
-  "text-blue-300",
   "text-green-300",
-  "text-yellow-300",
-  "text-purple-300",
-  "text-indigo-300",
-  "text-red-300",
-  "text-teal-300",
-  "text-orange-300",
-  "text-cyan-300",
 ];
 
 export function getColorForUser(userId: string): string {
